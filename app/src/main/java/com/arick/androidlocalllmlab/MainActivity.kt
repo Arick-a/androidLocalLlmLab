0d0736858d1cc66f3d4b550ce680c2e1dc57340d
Arick <1031900805@qq.com>
2026-08-04 17:42:47 +0800

    fix:test

--- app/src/main/java/com/arick/androidlocalllmlab/MainActivity.kt
+++ app/src/main/java/com/arick/androidlocalllmlab/MainActivity.kt
@@ -27,7 +27,6 @@ import androidx.compose.material.icons.filled.ArrowUpward
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
